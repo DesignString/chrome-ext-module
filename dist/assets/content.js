98619933836ad7cd8c45a4c7eaccb5a651145100
Punith B M <punith@designstring.com>
2022-07-20 19:53:03 +0530

    :art: content script issue

--- dist/assets/content.js
+++ dist/assets/content.js
@@ -1,1 +1,1 @@
-try{console.log("content loaded")}catch(o){console.error(o)}
+import{b as e}from"./browser-polyfill.f4591fc4.js";e.windows.getCurrent().then(o=>{console.log("window current data: ",o)});try{console.log("content loaded")}catch(o){console.error(o)}
--- src/content.js
+++ src/content.js
@@ -1,3 +1,9 @@
+import browser from "webextension-polyfill";
+
+browser.windows.getCurrent().then((window) => {
+    console.log("window current data: ", window);
+});
+
 try {
     console.log("content loaded");
 } catch (e) {
